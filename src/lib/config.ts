610f8451d7026cd608ba119ab5e071044588d832
Asif Ahammed <asifahammed359@gmail.com>
2025-08-10 23:05:11 +0600

    fix image .

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
-// export const NEXT_PUBLIC_API_BASE_URL = "https://medicale-ai-backend.onrender.com";
+export const NEXT_PUBLIC_API_BASE_URL = "https://medicale-ai-backend.onrender.com";
 
-export const NEXT_PUBLIC_API_BASE_URL = "http://localhost:8000"
+// export const NEXT_PUBLIC_API_BASE_URL = "http://localhost:8000"
 
 
 export const STORAGE_KEYS = {
